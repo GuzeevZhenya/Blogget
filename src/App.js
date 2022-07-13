a8b165aabaf6326e5011b0a1622d36413b8b5c8d
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-07-13 23:09:10 +0300

    create hygen function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
      <p>Hello</p>
+     <Button text="Жми меня "/>
     </div>
   );
 }
