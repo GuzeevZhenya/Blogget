c1e1ad64e90fc3740b343d49fe117e15e64e71d3
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-07-25 11:28:00 +0300

    add name

--- src/components/Main/List/Post/FormComments/FormComments.jsx
+++ src/components/Main/List/Post/FormComments/FormComments.jsx
@@ -2,9 +2,12 @@ import style from './FormComments.module.css';
 import {useState} from 'react';
 import {commentContext} from '../../../../../context/commentsContext';
 import {useContext} from 'react';
+import {authContext} from '../../../../../context/authContext';
 export const FormComment = () => {
   const [isTextArea, setIsTextArea] = useState(false);
   const {value, setValue} = useContext(commentContext);
+  const {auth} = useContext(authContext);
+  console.log(auth);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -29,6 +32,7 @@ export const FormComment = () => {
         </button>
       ) : (
         <form className={style.form} onSubmit={handleSubmit}>
+          <h2>{auth.name}</h2>
           <textarea className={style.textarea}
             value={value} onChange={handleChange}
           />
