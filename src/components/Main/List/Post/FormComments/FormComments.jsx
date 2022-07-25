0ef5a2ecf84dd15afd2a3d0ece446991ebb6a86c
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-07-25 21:44:47 +0300

    done

--- src/components/Main/List/Post/FormComments/FormComments.jsx
+++ src/components/Main/List/Post/FormComments/FormComments.jsx
@@ -1,41 +1,35 @@
 import style from './FormComments.module.css';
-import {useState} from 'react';
-import {commentContext} from '../../../../../context/commentsContext';
-import {useContext} from 'react';
-import {authContext} from '../../../../../context/authContext';
+import {useRef, useState} from 'react';
+
 export const FormComment = () => {
+  const inputRef = useRef(null);
+  // const focusRef = useRef(null);
   const [isTextArea, setIsTextArea] = useState(false);
-  const {value, setValue} = useContext(commentContext);
-  const {auth} = useContext(authContext);
-  console.log(auth);
 
   const handleSubmit = e => {
     e.preventDefault();
+    console.log(inputRef.current.value);
   };
 
   const handleClick = e => {
     setIsTextArea(true);
+    setTimeout(() => {
+      inputRef.current.focus();
+    }, 0);
   };
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
 
   return (
     <>
       {!isTextArea ? (
         <button
-          value={value}
           className={style.btn}
           onClick={handleClick}>
           Написать комментарий
         </button>
       ) : (
         <form className={style.form} onSubmit={handleSubmit}>
-          <h2>{auth.name}</h2>
-          <textarea className={style.textarea}
-            value={value} onChange={handleChange}
-          />
+          <textarea className={style.textarea} ref={inputRef}/>
           <button type='submit'
             className={style.btn}
           >
